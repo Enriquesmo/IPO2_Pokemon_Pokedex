48ee45f3e69ed74b13e24095592a6747c052385b
Enrique Sánchez-Migallón Ochoa <Enrique.Sanchez9@alu.uclm.es>
2023-03-09 17:33:55 +0100

    Sesión 5 - Kike - Versión 5.0 - Actualización de los atributos del pokemon Sandshrew para poder adaptarse al UserControl y tener acceso a todo lo que ofrece este. Hay que eliminar sus botones.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,8 +26,44 @@ namespace IPO2_Pokemon_Pokedex
         public MainPage()
         {
             this.InitializeComponent();
-            Sandshrew.Vida = 100;
-            //Sandshrew.herirPokemon(40);
+            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
+            SystemNavigationManager.GetForCurrentView().BackRequested += opcionVolver;
+            PokemonSandshrew.VerFondo = false;
+            PokemonSandshrew.VerNombre = false;
+            PokemonSandshrew.VerVida = false;
+            PokemonSandshrew.VerEnergia = false;
+        }
+
+
+        private void btn_Inicio_Click(object sender, RoutedEventArgs e)
+        {
+            FrameMain.Navigate(typeof(MainPage));
+            // ARREGLAR PORQUE SE DUPLICA
+        }
+
+        private void btn_Pokedex_Click(object sender, RoutedEventArgs e)
+        {
+            FrameMain.Navigate(typeof(PokedexPage), this); // Este this es para que se le pase como parámetro a la pagina creada, la pagina anterior
+        }
+
+        private void btn_CombatePokemon_Click(object sender, RoutedEventArgs e)
+        {
+            FrameMain.Navigate(typeof(CombatePage), this); // Este this es para que se le pase como parámetro a la pagina creada, la pagina anterior
+        }
+
+        private void btn_AcercaDe_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        /**/
+        private void opcionVolver(object sender, BackRequestedEventArgs e)
+        {
+            if (FrameMain.BackStack.Any())
+            {
+                FrameMain.GoBack();
+            }
+            // Que el boton de hacia atras vuelva tras la primera accion
         }
     }
 }
